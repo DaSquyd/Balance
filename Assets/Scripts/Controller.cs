e091ccff87e08efafa4b38e27c9676902b66a112
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2020-01-08 17:20:21 -0500

    1/8
    Added ball and movement, hotdog and destruction

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0f, 0f, Space.World);
+        transform.Rotate(0f, 0f, Input.GetAxis("Horizontal") * -10, Space.World);
+    }
+}
