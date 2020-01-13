87589b346c1af218c3b904f544e45c525d940a4a
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2020-01-13 17:19:44 -0500

    1/13
    Added countdown timer, textures, and colors

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
 
+    public Text countdown;
+    public Image clock;
+
     public float speed;
+
+    private float count = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +24,20 @@ public class Controller : MonoBehaviour
     {
         transform.Translate(Input.GetAxis("Horizontal") * speed, 0f, 0f, Space.World);
         transform.Rotate(0f, 0f, Input.GetAxis("Horizontal") * -10, Space.World);
+
+        countdown.text = Mathf.CeilToInt(10f - Time.time).ToString();
+        clock.fillAmount = count / 10f;
+        clock.color = Color.Lerp(Color.green, Color.red, count / 10f);
+
+        count -= Time.deltaTime;
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+#if !UNITY_EDITOR
+            Application.Quit();
+#else
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
